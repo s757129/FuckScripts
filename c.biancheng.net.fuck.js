7a01c441cd604a2d8e9da232a5ec0c8a325bce51
s757129 <92375768+s757129@users.noreply.github.com>
2022-01-19 13:19:29 +0800

    Update c.biancheng.net.fuck.js

--- c.biancheng.net.fuck.js
+++ c.biancheng.net.fuck.js
@@ -1,14 +1,15 @@
 // ==UserScript==
-// @name         C语言中文网隐藏VIP去广告
-// @namespace    https://github.com/s757129/Tampermonkey/blob/main/c.biancheng.net.fuck.js
-// @version      0.0.1
-// @description  * try to take over the world!
+// @name         C语言中文网移除VIP与广告
+// @namespace    https://github.com/s757129/Tampermonkey
+// @version      0.0.2
 // @author       s757129
 // @include      *://c.biancheng.net*
 // @icon         http://c.biancheng.net/favicon.ico
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @grant        GM_addStyle
 // @grant        unsafeWindow
+// @description  【0.0.2】/1.蒙查查修复一些问题/2.修改脚本名称与描述
+// @description  【0.0.1】/1.阻止新建标签页/2.移除顶栏VIP相关内容/3.移除文中引流广告/4.移除文中VIP相关内容/5.精简旧版首页/6.移除部分页脚内容/7.新版首页直接跳转至旧版首页
 // ==/UserScript==
 
 (function () {
@@ -17,10 +18,10 @@
     // 阻止新建标签页
     $('a').removeAttr("target")
 
-    // 移除顶部VIP栏
+    // 移除顶部VIP相关栏
     $('#topbar').remove();
 
-    // 移除个人中心图标
+    // 移除顶栏个人中心图标
     $('.user-info').remove();
 
     // 移除文中关注引流
@@ -31,10 +32,9 @@
     $('div.left').remove();
     $('img.right').remove();
 
-    // 隐藏VIP功能
+    // 移除VIP相关内容
     $('.vip').remove();
-    //$('a[href*="view/vip"]').parent().remove();
-    $('.glyphicon').parent().remove();
+    $('.glyphicon-usd').parent().remove();
     $('#ad-arc-top').remove();
     $('blockquote').remove();
     if($('.tip-box').val("高级教程"))
@@ -42,7 +42,7 @@
         $('.tip-box').remove();
     }
 
-    // 新版首页直接跳转
+    // 新版首页直接跳转至旧版首页
     var localhref = unsafeWindow.location;
     if(localhref.href.indexOf('c.biancheng.net/cpp')!=-1)
     {
