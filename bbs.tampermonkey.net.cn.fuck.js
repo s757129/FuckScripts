9a7e24ab9b5d5cc66be56c9cc32f01f0a383d1bb
柒伍七 <92375768+s757129@users.noreply.github.com>
2022-02-17 16:27:31 +0800

    Update bbs.tampermonkey.net.cn.fuck.js

--- bbs.tampermonkey.net.cn.fuck.js
+++ bbs.tampermonkey.net.cn.fuck.js
@@ -17,5 +17,4 @@
     GM_addStyle('.a_f{display:none; !important}');
     GM_addStyle('.a_p{display:none; !important}');
     GM_addStyle('.mn{width:100%; height:100%; left:0; top:0; right:0; bottom: 0; margin: auto;}');
-
 })();
