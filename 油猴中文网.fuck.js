7d3e3e83b09d3bd3530e09ac200afedb23e1ec9c
柒伍七 <92375768+s757129@users.noreply.github.com>
2022-02-17 17:11:44 +0800

    Update 油猴中文网.fuck.js

--- 油猴中文网.fuck.js
+++ 油猴中文网.fuck.js
@@ -2,7 +2,7 @@
 // @name         去他の油猴中文网
 // @namespace    https://github.com/s757129/Tampermonkey
 // @version      1.0.0
-// @description  try to take over the world!!
+// @description  屏蔽油猴中文网广告加菊部美化
 // @author       柒伍七
 // @include      http*://bbs.tampermonkey.net.cn*
 // @icon         https://bbs.tampermonkey.net.cn/favicon.ico
@@ -18,7 +18,7 @@
     GM_addStyle('.a_f{display:none; !important}');
     GM_addStyle('.a_p{display:none; !important}');
     
-    //资料页
+    //美化
     GM_addStyle('.mn{width:100%; height:100%; left:0; top:0; right:0; bottom: 0; margin: auto;}');
 
 })();
