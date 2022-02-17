c52a0111651fa3d2a32d26b7d16d731d1e9d737f
柒伍七 <92375768+s757129@users.noreply.github.com>
2022-02-17 16:31:55 +0800

    Update bbs.tampermonkey.net.cn.fuck.js

--- bbs.tampermonkey.net.cn.fuck.js
+++ bbs.tampermonkey.net.cn.fuck.js
@@ -4,7 +4,7 @@
 // @version      1.0.0
 // @description  try to take over the world!!
 // @author       柒伍七
-// @include      *bbs.tampermonkey.net.cn*
+// @include      http*://bbs.tampermonkey.net.cn*
 // @icon         https://bbs.tampermonkey.net.cn/favicon.ico
 // @run-at       document-start
 // @grant        GM_addStyle
@@ -17,4 +17,5 @@
     GM_addStyle('.a_f{display:none; !important}');
     GM_addStyle('.a_p{display:none; !important}');
     GM_addStyle('.mn{width:100%; height:100%; left:0; top:0; right:0; bottom: 0; margin: auto;}');
+
 })();
