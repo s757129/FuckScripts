698aac5050b15003b11c5762d23f1ec253d1e86f
柒伍七 <92375768+s757129@users.noreply.github.com>
2022-04-06 22:34:33 +0800

    Update 油猴中文网.fuck.user.js

--- 油猴中文网.fuck.user.js
+++ 油猴中文网.fuck.user.js
@@ -4,7 +4,7 @@
 // @version      1.0.1
 // @description  屏蔽油猴中文网广告加菊部美化
 // @author       柒伍七
-// @include      http*://bbs.tampermonkey.net.cn*
+// @include      *://bbs.tampermonkey.net.cn*
 // @icon         https://bbs.tampermonkey.net.cn/favicon.ico
 // @run-at       document-start
 // @grant        unsafeWindow
